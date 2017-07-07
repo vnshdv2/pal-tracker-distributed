6f5ee2da6acdc93c14c7c5ae66e5e7f51df2bd32
Tyson Gern <tgern@pivotal.io>
2017-07-07 09:52:54 -0600

    serity-start

---
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.tracker.allocations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestOperations;
+
+import java.util.TimeZone;
+
+
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+@ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProjectClient projectClient(
+        RestOperations restOperations,
+        @Value("${registration.server.endpoint}") String registrationEndpoint
+    ) {
+        return new ProjectClient(restOperations, registrationEndpoint);
+    }
+}
