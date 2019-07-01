9059bf9b0bdad24e40a168b711a547985d3ef4c8
vnshdv2 <vnshdv2@gmail.com>
2019-07-01 14:11:31 +0530

    deleting all files

--- buildSrc/src/main/groovy/io/pivotal/pal/tracker/gradlebuild/DatabasesExtension.groovy
+++
@@ -1,8 +0,0 @@
-package io.pivotal.pal.tracker.gradlebuild
-
-class DatabasesExtension {
-    String devDatabase
-    String testDatabase
-    String cfDatabase
-    String cfApp
-}
--- databases/backlog-database/migrations/V1__initial_schema.sql
+++
@@ -1,9 +0,0 @@
-create table stories (
-  id         bigint(20) not null auto_increment,
-  project_id bigint(20),
-  name       VARCHAR(255),
-
-  primary key (id)
-)
-engine = innodb
-default charset = utf8;
